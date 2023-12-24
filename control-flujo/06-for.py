742de105c2525feb186ff8001798dd7303bad45f
heri <heri_espino@outlook.com>
2023-12-24 06:15:07 -0600

    hola

--- control-flujo/06-for.py
+++ control-flujo/06-for.py
@@ -5,6 +5,19 @@ for numero in range(5):
 
 buscar = 3
 for numero in range(5):
-    if numero == buscar:
+    if numero == buscar:  # recuerda que es ==
         print("\n")
         print(numero, numero * "jaja ")
+        break  # detiene lo que sigue del codigo
+
+buscar = 10
+for numero in range(5):
+    if numero == buscar:  # recuerda que es ==
+        print("\n")
+        print(numero, numero * "jaja ")
+        break  # detiene lo que sigue del codigo
+else:
+    print("no está el número buscado :c")
+
+for char in "Te amo, Bad Bunny":
+    print(char)
